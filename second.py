ea6e9a7c82f98a775a79eb4346597181701a0d17
seema <seema.tamang640@gmail.com>
2018-02-11 11:42:49 +0545

    To take the first name and lastname of user and write email according to the first and last name.

---
+++ second.py
@@ -0,0 +1,23 @@
+class student:
+    def __init__(self,fstr,lstr):
+            self.fstr=fstr
+            self.lstr=lstr
+
+    def get_string(self):
+        self.fstr=fstr
+    def get_string2(self):
+        self.lstr=lstr
+    def update_firstname(self):
+        print("Dear {},I would like to ............".format(self.fstr))
+
+fstr=input("Enter your firstname:")
+lstr=input("Enter your lastname:")
+s=student("fstr","lstr")
+s.get_string()
+s.get_string2()
+s.update_firstname()
+
+
+
+
+
